da3ff515efe8ebd8ea770deb15805e812cbcc140
John Ericson <John.Ericson@Obsidian.Systems>
2018-09-21 13:57:34 -0400

    atoi64 needs to be *static* inline

--- bitcoinAmount.c
+++ bitcoinAmount.c
@@ -28,7 +28,7 @@
 static const int64_t CENT = 1000000;
 static const int64_t COIN = 100000000;
 
-inline int64_t atoi64(const char* psz)
+static inline int64_t atoi64(const char* psz)
 {
 #ifdef _MSC_VER
     return _atoi64(psz);
